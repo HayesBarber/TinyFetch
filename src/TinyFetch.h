c6cadd6e17d70bd28f3eab4ead7d436c10106e5e
Hayes Barber <barberhayes@gmail.com>
2025-07-06 09:38:05 -0400

    create struct

--- src/TinyFetch.h
+++ src/TinyFetch.h
@@ -1,11 +1,18 @@
 #ifndef TINY_FETCH_H
 #define TINY_FETCH_H
 
+#include <Arduino.h>
 #include <HTTPClient.h>
+
+struct HttpResponse {
+    int statusCode = -1;
+    String payload;
+};
 
 class TinyFetch {
 public:
-    TinyFetch();
+    TinyFetch(const String& url);
+    HttpResponse get(const String& path);
 
 private:
     String _baseUrl;
